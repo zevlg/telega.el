4457087e1e34e7340d3678714807335665d798d8
Zajcev Evgeny <zevlg@yandex.ru>
2019-09-12 10:00:36 +0300

    Version -> 0.4.4

--- server/telega-server.c
+++ server/telega-server.c
@@ -42,7 +42,7 @@ void pngext_main(int ac, char** av);
 
 char* logfile = NULL;
 int verbosity = 5;
-const char* version = "0.4.3";
+const char* version = "0.4.4";
 
 /* true when tdlib_loop is running */
 volatile bool tdlib_running;
