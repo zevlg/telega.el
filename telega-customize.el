710625d78e5487f78eb18b8050265bf7c570a4c9
Alex Murray <murray.alex@gmail.com>
2019-11-28 21:23:09 +1030

    Use better check for new emacs image handling implementation

--- telega-customize.el
+++ telega-customize.el
@@ -129,7 +129,8 @@ Make sure you have tracking.el loaded if this option is enabled."
   :type 'boolean
   :group 'telega
   :set (lambda (option value)
-         (if (or (fboundp 'imagemagick-types) (version< "27.0.49" emacs-version))
+         (if (or (fboundp 'imagemagick-types)
+                 (and (fboundp 'image-transforms-p) (funcall 'image-transforms-p)))
              (set-default option value)
            (set-default option nil)
            (when value
