b1f16096cd64b124362ef02cd257497a898f7045
Zajcev Evgeny <zevlg@yandex.ru>
2020-12-23 01:16:42 +0300

    Fix compilation of some .el files
    - contrib/telega-live-location.el: Avoid loading `geo' at compile time
    - docs/index-html-gen.el: Do not compile this file

--- docs/index-html-gen.el
+++ docs/index-html-gen.el
@@ -1,6 +1,7 @@
 (require 'org)
 (require 'ox)
 (require 'ellit-org)
+(require 'telega)
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((shell . t)
@@ -10,6 +11,14 @@
 (setq org-confirm-babel-evaluate nil)
 
 (setq org-export-with-broken-links 'mark)
+
+;; 
+(let ((toc-entry (assoc "Table of Contents" org-export-dictionary)))
+  (setcdr toc-entry
+          (cons (list "telega" :html (format "<img width=20 height=20 src=\"https://raw.githubusercontent.com/zevlg/telega.el/master/etc/telega-logo.svg\"/>&nbsp;Telega Manual (v%s)" telega-version))
+                (cdr toc-entry))))
+
+(setq org-export-default-language "telega")
 
 (defun telega-org-title-gen-custom-id (origfun datum)
   (let ((ret (funcall origfun datum)))
@@ -23,3 +32,7 @@
 
 (advice-add 'org-export-search-cells
             :around 'telega-org-title-gen-custom-id)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
