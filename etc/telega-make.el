966acc89f1f0182fa59be5edc588925264f6aea4
Zajcev Evgeny <zevlg@yandex.ru>
2021-02-09 14:31:17 +0300

    [add] etc/telega-make.el: transient package added as dependency

--- etc/telega-make.el
+++ etc/telega-make.el
@@ -27,7 +27,7 @@
   (package-initialize)
 
   (let* ((core-pkgs '(visual-fill-column rainbow-identifiers))
-         (contrib-pkgs '(all-the-icons alert dashboard))
+         (contrib-pkgs '(all-the-icons alert dashboard transient))
          (all-pkgs (append core-pkgs contrib-pkgs))
          (need-pkgs (cl-remove-if #'package-installed-p all-pkgs)))
     (when need-pkgs
