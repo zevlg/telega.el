64b25ea05a1752b6133b02f09c37729de62aa7e8
Zajcev Evgeny <zevlg@yandex.ru>
2022-01-28 16:35:31 +0300

    [fix] Correctly set `telega--sort-reorder-dirtiness'
    When new sorting criteria is applied by
    `telega-sort-set-active-criteria', set
    `telega--sort-reorder-dirtiness' correctly to avoid runtime errors in
    the future.

--- telega-sort.el
+++ telega-sort.el
@@ -153,9 +153,9 @@ overwriting currently active one."
     (setq telega--sort-criteria criteria)
     (setq telega--sort-inverted inverted)
     (setq telega--sort-reorder-dirtiness
-          (apply #'append (lambda (criteria-sym)
-                            (get criteria-sym :telega-order-events))
-                 telega--sort-criteria))
+          (apply #'append (mapcar (lambda (criteria-sym)
+                                    (get criteria-sym :telega-order-events))
+                                  telega--sort-criteria)))
 
     ;; NOTE: compare function might do weird things, so
     ;; `copy-sequence' is used
