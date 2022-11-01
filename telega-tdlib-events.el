d9772d66e8cca8f43b8f7fd9dc814a4fff1f539d
mANIApHOBIC <git@maniaphobic.org>
2022-10-31 20:48:03 -0700

    Correct spelling of variable "discad" to "discard"
    I renamed the misspelled variable "discad" to "discard" in the
    telega--on-updateCall function (in telega-tdlib-events.el)

--- telega-tdlib-events.el
+++ telega-tdlib-events.el
@@ -895,10 +895,10 @@ messages."
                   (telega-user--name user) (plist-get err :message))))
 
       (callStateDiscarded
-       (let ((discad (plist-get state :reason))
+       (let ((discard (plist-get state :reason))
              (user (telega-user-get (plist-get call :user_id))))
          (message "Call %s discarded: %s" (telega-user--name user)
-                  (substring (plist-get discad :@type) 17))))
+                  (substring (plist-get discard :@type) 17))))
       )
 
     ;; Delete call from the list, if call is ended
