35c26aa9b88999e631cf0b7842fbd42219b1d2dc
Wang Liao <lwlw1988@gmail.com>
2026-04-14 14:12:57 +0800

    [codex] Fix forum topic lookup from topic messages (#567)
    * Fix forum topic lookup from topic messages
    
    * fix(topic): assert on TDLib errors in topic sync

--- telega-msg.el
+++ telega-msg.el
@@ -1230,7 +1230,8 @@ corresponding thread or topic."
                  ;; is already ordinary supergroup, in this case we
                  ;; can't get topic
                  (chat is-forum)))
-         (telega-topic-goto (telega-msg-topic msg) (plist-get msg :id)))
+        (telega-topic-goto (telega-msg-topic msg 'sync)
+                           (plist-get msg :id)))
 
         ((telega-msg-match-p msg 'post-with-comments)
          (telega-chat--goto-thread (telega-msg-chat msg 'offline)
--- test.el
+++ test.el
@@ -282,6 +282,111 @@ Have Stoploss 690 Satoshi." :entities []))))
     (should (equal (external-completion--all-completions "you" table nil 3)
                    '("i-love-you")))))
 
+(ert-deftest telega-bot-chat-with-topics-is-forum ()
+  "Bot chats with topics enabled should reuse forum topic support."
+  (let* ((bot-id 90901)
+         (chat-id 90902)
+         (users-ht (cdr (assq 'user telega--info)))
+         (bot-user `(:@type "user" :id ,bot-id
+                             :first_name "Topic"
+                             :last_name "Bot"
+                             :type (:@type "userTypeBot" :has_topics t)))
+         (bot-chat `(:@type "chat" :id ,chat-id
+                             :type (:@type "chatTypePrivate" :user_id ,bot-id)
+                             :title "Topic Bot")))
+    (unwind-protect
+        (progn
+          (puthash bot-id bot-user users-ht)
+          (puthash chat-id bot-chat telega--chats)
+          (should (telega-chat-match-p bot-chat 'is-forum)))
+      (remhash bot-id users-ht)
+      (remhash chat-id telega--chats))))
+
+(ert-deftest telega-msg-open-thread-or-topic-fetches-forum-topic ()
+  "Opening a forum topic message should fetch missing topic info."
+  (let* ((bot-id 90911)
+         (chat-id 90912)
+         (topic-id 90913)
+         (msg-id 90914)
+         (users-ht (cdr (assq 'user telega--info)))
+         (bot-user `(:@type "user" :id ,bot-id
+                             :first_name "Topic"
+                             :last_name "Bot"
+                             :type (:@type "userTypeBot" :has_topics t)))
+         (bot-chat `(:@type "chat" :id ,chat-id
+                             :type (:@type "chatTypePrivate" :user_id ,bot-id)
+                             :title "Topic Bot"))
+         (msg `(:@type "message" :id ,msg-id :chat_id ,chat-id
+                         :topic_id (:@type "messageTopicForum"
+                                           :forum_topic_id ,topic-id)))
+         (forum-topic `(:@type "forumTopic"
+                                :info (:@type "forumTopicInfo"
+                                              :chat_id ,chat-id
+                                              :forum_topic_id ,topic-id
+                                              :icon (:@type "forumTopicIcon"
+                                                            :color 0
+                                                            :custom_emoji_id "0")
+                                              :name "General"))))
+    (unwind-protect
+        (progn
+          (puthash bot-id bot-user users-ht)
+          (puthash chat-id bot-chat telega--chats)
+          (cl-letf (((symbol-function 'telega--getForumTopic)
+                     (lambda (chat forum-topic-id &optional _callback)
+                       (should (eq chat bot-chat))
+                       (should (= forum-topic-id topic-id))
+                       forum-topic))
+                    ((symbol-function 'telega-topic-goto)
+                     (lambda (topic start-msg-id)
+                       (should (eq (telega--tl-type topic) 'forumTopic))
+                       (should (eq topic (telega-topic-get bot-chat topic-id)))
+                       (should (= start-msg-id msg-id))
+                       'topic-opened)))
+            (should (eq (telega--tl-type (telega-msg-topic msg 'sync)) 'forumTopic))
+            (should (eq (telega-msg-topic msg 'sync)
+                        (telega-topic-get bot-chat topic-id)))
+            (should (eq (telega-msg-open-thread-or-topic msg) 'topic-opened))
+            (should (eq (telega--tl-type (telega-msg-topic msg)) 'forumTopic))
+            (should (eq (telega-msg-topic msg)
+                        (telega-topic-get bot-chat topic-id)))))
+      (remhash chat-id telega--chat-topics)
+      (remhash bot-id users-ht)
+      (remhash chat-id telega--chats))))
+
+
+(ert-deftest telega-msg-topic-sync-asserts-on-forum-topic-errors ()
+  "Sync forum topic lookup should assert on internal TDLib errors."
+  (let* ((bot-id 90921)
+         (chat-id 90922)
+         (topic-id 90923)
+         (users-ht (cdr (assq 'user telega--info)))
+         (bot-user `(:@type "user" :id ,bot-id
+                             :first_name "Topic"
+                             :last_name "Bot"
+                             :type (:@type "userTypeBot" :has_topics t)))
+         (bot-chat `(:@type "chat" :id ,chat-id
+                             :type (:@type "chatTypePrivate" :user_id ,bot-id)
+                             :title "Topic Bot"))
+         (msg `(:@type "message" :id 90924 :chat_id ,chat-id
+                         :topic_id (:@type "messageTopicForum"
+                                           :forum_topic_id ,topic-id))))
+    (unwind-protect
+        (progn
+          (puthash bot-id bot-user users-ht)
+          (puthash chat-id bot-chat telega--chats)
+          (cl-letf (((symbol-function 'telega--getForumTopic)
+                     (lambda (_chat _forum-topic-id &optional _callback)
+                       '(:@type "error" :code 404 :message "Not Found"))))
+            (let ((debug-on-error nil))
+              (should-error (telega-msg-topic msg 'sync)
+                            :type 'cl-assertion-failed)
+              (should-error (telega-msg-open-thread-or-topic msg)
+                            :type 'cl-assertion-failed)
+              (should-error (telega-msg-show-topic-info msg)
+                            :type 'cl-assertion-failed))))
+      (remhash chat-id telega--chat-topics)
+      (remhash bot-id users-ht)
+      (remhash chat-id telega--chats))))
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
